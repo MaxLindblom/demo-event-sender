92ed591dea3f837106af59b40409e947bf22b668
MaxLindblom <max.g.lindblom@gmail.com>
2020-10-30 14:09:08 +0100

    Cleanup and documentation

--- src/util/eventSender.js
+++ src/util/eventSender.js
@@ -1,6 +1,10 @@
-const ENDPOINT_URL = 'TODO: Add URL here' // TODO: Add URL here
+const ENDPOINT_URL = 'TODO: Add API key here' // TODO: Add URL here
 const API_KEY = 'TODO: Add API key here' // TODO: Add API key here
 
+/**
+ * Note: Below method for sending events is janky, and will not work in browsers with CORS enabled (all of them probably)
+ * See README for more info
+ */
 const sendStartEvent = (metadata, payload) => {
   const data = {
     'event_name': 'start_session',
@@ -10,17 +14,30 @@ const sendStartEvent = (metadata, payload) => {
 
   try {
     const encodedBody = window.btoa(JSON.stringify(data));
-    fetch(`${ENDPOINT_URL}/send_data?data=${encodedBody}`, {
-      mode: 'no-cors',
-      headers: {
-        'x-api-key': API_KEY
+    const invocation = new XMLHttpRequest();
+    invocation.open('GET', `${ENDPOINT_URL}send_data?data=${encodedBody}`, true)
+    invocation.setRequestHeader('x-api-key', API_KEY)
+    invocation.onreadystatechange = function () {
+      if(invocation.readyState === XMLHttpRequest.DONE) {
+        var status = invocation.status;
+        if (status === 0 || (status >= 200 && status < 400)) {
+          // Handle event - in this case, just log it to console
+          console.log(invocation.responseText);
+        } else {
+          // Oh no! There has been an error with the request!
+        }
       }
-    });
+    };
+    invocation.send()
   } catch (error) {
     throw Error(`Failed to send start event`);
   }
 }
 
+/**
+ * Note: Below method for sending events is janky, and will not work in browsers with CORS enabled (all of them probably)
+ * See README for more info
+ */
 const sendTestEvent = (metadata, payload) => {
   const data = {
     'event_name': 'test_event',
@@ -30,12 +47,22 @@ const sendTestEvent = (metadata, payload) => {
 
   try {
     const encodedBody = window.btoa(JSON.stringify(data));
-    fetch(`${ENDPOINT_URL}/send_data?data=${encodedBody}`, {
-      mode: 'no-cors',
-      headers: {
-        'x-api-key': API_KEY
+    const invocation = new XMLHttpRequest();
+    invocation.open('GET', `${ENDPOINT_URL}send_data?data=${encodedBody}`, true)
+    invocation.setRequestHeader('x-api-key', API_KEY)
+    invocation.onreadystatechange = function () {
+      // In local files, status is 0 upon success in Mozilla Firefox
+      if(invocation.readyState === XMLHttpRequest.DONE) {
+        var status = invocation.status;
+        if (status === 0 || (status >= 200 && status < 400)) {
+          // Handle event - in this case, just log it to console
+          console.log(invocation.responseText);
+        } else {
+          // Oh no! There has been an error with the request!
+        }
       }
-    });
+    };
+    invocation.send()
   } catch (error) {
     throw Error(`Failed to send test event`);
   }
